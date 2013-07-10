680d17ffa347d1cf4691c96be38bf3389bb04c41
Berliner1985 <cbrieger@brieger.biz>
2013-07-10 00:35:52 +0000

    Updated web.js (buffer and index.html)

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// create buffer variable
+// read file and assign to buffer variable
+var buffer = new Buffer(stats.size);
+var fs = require("fs");
+buffer = fs.readFileSync("index.html", "utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
